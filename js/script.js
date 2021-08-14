0476443efdb582a75fec26f0dcc1c200ff199993
Santosh <pantsantosh23@gmail.com>
2021-08-14 22:51:37 +0545

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+var slides = document.querySelectorAll(".slideimg");
+var btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
+
+// Javascript for image slider manual navigation
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+// Javascript for image slider autoplay navigation
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function () {
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
+
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 10000);
+  };
+  repeater();
+};
+repeat();
+
+burger = document.querySelector(".burger");
+navbar = document.querySelector(".navbar");
+navlist = document.querySelector(".ul");
+rightnav = document.querySelector(".right");
+burger.addEventListener("click", () => {
+  rightnav.classList.toggle("v-class-resp");
+  navlist.classList.toggle("v-class-resp");
+  navbar.classList.toggle("h-nav-resp");
+});
+
+// confirm("Are you sure you want to delete");
+// alert("Hacked");
